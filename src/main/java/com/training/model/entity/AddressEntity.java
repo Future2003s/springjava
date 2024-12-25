cec9d77ea495d1eece6b3c0d7b349fbca41a2785
MinhYi <minhyii.dev@gmail.com>
2024-12-25 10:54:30 +0700

    add addressEntity

--- src/main/java/com/training/model/entity/AddressEntity.java
+++ src/main/java/com/training/model/entity/AddressEntity.java
@@ -1,4 +1,37 @@
 package com.training.model.entity;
 
+
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+import org.springframework.boot.sql.init.dependency.DependsOnDatabaseInitialization;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Table(name = "tbl_address")
 public class AddressEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    String apartmentNumber;
+
+    String floor;
+
+    String building;
+
+    String streetNumber;
+
+    String street;
+
+    String city;
+
+    String country;
+
+    Integer addressType;
 }
