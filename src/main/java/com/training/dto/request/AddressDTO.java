126b22d42f6b341c0ac76bf1ac2b7219dbd23b47
MinhYi <minhyii.dev@gmail.com>
2024-12-25 10:43:46 +0700

    add AddressEnity

--- src/main/java/com/training/dto/request/AddressDTO.java
+++ src/main/java/com/training/dto/request/AddressDTO.java
@@ -1,7 +1,33 @@
 package com.training.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.experimental.FieldDefaults;
+
 import java.io.Serializable;
 
+@Getter
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class AddressDTO implements Serializable {
+    @NotBlank(message = "floor not allow empty !")
+    String floor;
 
+    @NotBlank(message = "apartmentNumber not allow empty !")
+    String apartmentNumber;
+
+    @NotBlank(message = "streetNumber not allow empty !")
+    String streetNumber;
+
+    @NotBlank(message = "address not allow empty !")
+    String address;
+
+    @NotBlank(message = "city not allow empty !")
+    String city;
+
+    @NotBlank(message = "city not allow empty !")
+    String country;
+
+    @NotBlank(message = "addressType not allow empty !")
+    Integer addressType;
 }
